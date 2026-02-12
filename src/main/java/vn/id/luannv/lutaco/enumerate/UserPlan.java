6018c179a878b6b63d66dc9f7db7f08c5a8da855
luannv <vanluan.dev@gmail.com>
2026-02-12 11:21:21 +0700

    init: add user plan spending limit count

--- src/main/java/vn/id/luannv/lutaco/enumerate/UserPlan.java
+++ src/main/java/vn/id/luannv/lutaco/enumerate/UserPlan.java
@@ -1,15 +1,42 @@
 package vn.id.luannv.lutaco.enumerate;
 
+import jakarta.annotation.PostConstruct;
 import lombok.AccessLevel;
 import lombok.Getter;
-import lombok.RequiredArgsConstructor;
 import lombok.experimental.FieldDefaults;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
 
 @Getter
-@RequiredArgsConstructor
-@FieldDefaults(level = AccessLevel.PRIVATE, makeFinal = true)
+@FieldDefaults(level = AccessLevel.PRIVATE)
 public enum UserPlan {
-    PREMIUM(5),
-    FREEMIUM(1);
+    PREMIUM,
+    FREEMIUM;
+
     int maxWallets;
+    int maxBudgetCount;
+
+    @Component
+    public static class ValueInjector {
+
+        @Value("${user-plan.premium-max-wallets}")
+        private int premiumMaxWallets;
+
+        @Value("${user-plan.premium-max-budget-count}")
+        private int premiumMaxBudgetCount;
+
+        @Value("${user-plan.freemium-max-wallets}")
+        private int freemiumMaxWallets;
+
+        @Value("${user-plan.freemium-max-budget-count}")
+        private int freemiumMaxBudgetCount;
+
+        @PostConstruct
+        public void postConstruct() {
+            PREMIUM.maxWallets = premiumMaxWallets;
+            PREMIUM.maxBudgetCount = premiumMaxBudgetCount;
+            FREEMIUM.maxWallets = freemiumMaxWallets;
+            FREEMIUM.maxBudgetCount = freemiumMaxBudgetCount;
+        }
+    }
 }
