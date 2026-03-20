d4f4abb5dc6b1a2def827a633baaba5b900cabac
luannv <vanluan.dev@gmail.com>
2026-03-20 23:17:38 +0700

    i18n for config enums const

--- src/main/java/vn/id/luannv/lutaco/enumerate/PeriodRange.java
+++ src/main/java/vn/id/luannv/lutaco/enumerate/PeriodRange.java
@@ -1,9 +1,19 @@
 package vn.id.luannv.lutaco.enumerate;
 
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import lombok.experimental.FieldDefaults;
+
+@Getter
+@FieldDefaults(makeFinal = true, level = AccessLevel.PRIVATE)
+@RequiredArgsConstructor
 public enum PeriodRange {
-    LAST_7_DAYS,
-    LAST_1_MONTH,
-    LAST_3_MONTHS,
-    LAST_1_YEAR,
-    ALL_TIME
+    LAST_7_DAYS("config.enum.period.range.last_7_days"),
+    LAST_1_MONTH("config.enum.period.range.last_1_month"),
+    LAST_3_MONTHS("config.enum.period.range.last_3_months"),
+    LAST_1_YEAR("config.enum.period.range.last_1_year"),
+    ALL_TIME("config.enum.period.range.all_time")
+    ;
+    String display;
 }
--- src/main/java/vn/id/luannv/lutaco/enumerate/UserStatus.java
+++ src/main/java/vn/id/luannv/lutaco/enumerate/UserStatus.java
@@ -1,8 +1,18 @@
 package vn.id.luannv.lutaco.enumerate;
 
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import lombok.experimental.FieldDefaults;
+
+@Getter
+@FieldDefaults(makeFinal = true, level = AccessLevel.PRIVATE)
+@RequiredArgsConstructor
 public enum UserStatus {
-    PENDING_VERIFICATION,
-    ACTIVE,
-    DISABLED_BY_USER,
-    BANNED
+    PENDING_VERIFICATION("config.enum.user.status.pending_verification"),
+    ACTIVE("config.enum.user.status.active"),
+    DISABLED_BY_USER("config.enum.user.status.disabled_by_user"),
+    BANNED("config.enum.user.status.banned")
+    ;
+    String display;
 }
